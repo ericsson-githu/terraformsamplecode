23eb01341a5a73cee87ef7d278ba929c45a566f5
uday pratap singh <pruday32@gmail.com>
2025-09-08 12:55:23 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,35 +1,17 @@
 module "rg" {
   source = "../modules/resourece_group"
-  rg     = "myResourceGroup"
-  location = "East US"
+  rg     = "myResourceavi"
+  location = "central india"
 }
 
 module "sg1" {
   
     source                  = "../modules/storage_account"
     sg                      = "mystorageaccount8uday"
-    rg                      = "myResourceGroup"
-    location                = "East US"
+    rg                      = "myResourceavi"
+    location                = "central india"
     account_tier            = "Standard"
     account_replication_type = "LRS"
     }
 
-    module "sg2" {
-  
-    source                  = "../modules/storage_account"
-    sg                      = "mystorageaccount9uday"
-    rg                      = "myResourceGroup"
-    location                = "East US"
-    account_tier            = "Standard"
-    account_replication_type = "LRS"
-    }
-
-    module "sg4" {
-  
-    source                  = "../modules/storage_account"
-    sg                      = "mystorageaccount11uday"
-    rg                      = "myResourceGroup"
-    location                = "East US"
-    account_tier            = "Standard"
-    account_replication_type = "LRS"
-    }
+ 
