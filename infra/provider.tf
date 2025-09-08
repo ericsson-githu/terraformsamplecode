ffdedfbf1f22baa793b5ff0ebf2f33f117682e81
uday pratap singh <pruday32@gmail.com>
2025-09-08 23:24:00 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "4.41.0"
     }
   }
-  #   backend "azurerm" {
-  #     resource_group_name  = ""
-  #     storage_account_name = ""
-  #     container_name       = ""
-  #     key                  = ""
-  #   }
+      backend "azurerm" {
+         resource_group_name  = "uday-rg"
+         storage_account_name = "terraformuday7231"
+         container_name       = "tfstatefile"
+         key                  = "prod.tfstate"
+       }
 }
 
 provider "azurerm" {
