ef633d19728ce9648ea7d64c22dc03c81c302a07
uday pratap singh <pruday32@gmail.com>
2025-09-08 11:48:12 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "uday-rg"   
     storage_account_name = "terraformuday7231"                       
-    container_name       = "tfstatefile"                        
+    container_name       = "tfstatefile1"                        
     key                  = "prod.tfstate"         
   }
 }
